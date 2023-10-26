9e2f1e6460fdbe4f26577f4d0e4df7fb8fef5351
Pierrot-jang-pilkyu <jangpilkyu1@gmail.com>
2023-10-26 16:18:17 +0900

    [Feat] Home image 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,16 @@
 import styles from "./Home.module.css";
 import Button from "./Button_8px.js";
 import Header from "./Header.js";
-// import Ball from "./img.png";
+import HomeBall from "./HomeBall.png";
 
 function Home()
 {
 	return (
 		<div className={`${styles.background}`}>
-			{/* <img className={`${styles.img}`} src={Ball}/> */}
+			<img className={`${styles.img}`} src={HomeBall}/>
 			<Header/>
 			<div className={`${styles.main}`}>
-				<div className={`${styles.title}`}>Deer Feer</div>
+				<div className={`${styles.title}`}>D or Feer  P__ong</div>
 				<Button name="login"/>
 			</div>
 		</div>
