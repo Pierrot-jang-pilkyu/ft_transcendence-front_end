253916208160eaeadc09f0fd41c0010e967d4d16
sunwchoi <chltjsdn103@naver.com>
2023-10-27 17:05:36 +0900

    Feat: Game 스크린 추가
    1. 스크린 추가
    
    resolved #8

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,8 +1,16 @@
+import Canvas from "./Canvas";
+import Header from "../../components/Header";
+import styles from "./Game.module.css";
 
 function Game()
 {
 	return (
-		<div>Test</div>
+		<div className={`${styles.background}`}>
+			<Header/>
+			<div className={`${styles.canvas}`}>
+				<Canvas/>
+			</div>
+		</div>
 	);
 }
 
