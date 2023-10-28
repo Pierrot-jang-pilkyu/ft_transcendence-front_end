ae12af176b2f247e0180b6f0db1fce3b54a91e42
jaeywon <dnsjwodyd@gmail.com>
2023-10-28 18:44:56 +0900

    타인프로필 버튼추가전

--- src/pages/Profile/Match History/MatchHistory.tsx
+++ src/pages/Profile/Match History/MatchHistory.tsx
@@ -6,7 +6,6 @@ function MatchHistory() {
         <div className={`${styles.Historycontainer}`}>
             <div className={`${styles.Historytitle}`}>Match History</div>
             <div className={`${styles.Historybackground}`}>
-                <div className={`${styles.Historytable}`}>
                     {/*밑에 부분에 백과 협의후 작성해야한다.*/}
                     <HistoryList winflag={true} matchtime={'2023.10.12  12:29:21'} name={'nickname'}/>
                     <HistoryList winflag={false} matchtime={'2023.10.12  12:29:21'} name={'nickname'}/>
@@ -24,7 +23,6 @@ function MatchHistory() {
                     <HistoryList winflag={false} matchtime={'2023.10.12  12:29:21'} name={'nickname'}/>
                     <HistoryList winflag={true} matchtime={'2023.10.12  12:29:21'} name={'nickname'}/>
                     <HistoryList winflag={true} matchtime={'2023.10.12  12:29:21'} name={'nickname'}/> 
-                </div>
             </div>
         </div>
     );
