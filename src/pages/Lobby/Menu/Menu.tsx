b723b69c6f10eca1e18c67d5da6c90a8de62a85f
Pierrot-jang-pilkyu <jangpilkyu1@gmail.com>
2023-10-31 19:16:57 +0900

    [Feat] Friends list avatar add
    Fixes:#12

--- src/pages/Lobby/Menu/Menu.tsx
+++ src/pages/Lobby/Menu/Menu.tsx
@@ -1,9 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import styles from "./Menu.module.css";
+import Avatar from '../Friends/Avatar';
 import GameStart from "../../../assets/GameStart.svg";
 import ChattingRoom from "../../../assets/Chatting.svg";
 import SearchFriends from "../../../assets/SearchFriends.svg";
 import FriendsArrow from "../../../assets/FriendsArrow.svg";
+import FriendsAdd from "../../../assets/FriendsAdd.svg";
 
 function Menu()
 {
@@ -30,24 +32,56 @@ function Menu()
 				<div className={`${styles.chat_font}`} onClick={handlerButtonFriends}>Chatting Room</div>
 		</button>
 		<div className="drawer drawer-end">
-            	<input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-				<div className="drawer-content">
-					{/* Page content here */}
-					<label htmlFor="my-drawer-4" className={`${styles.button} ${styles.friend} ${styles.friend_font}` }>
-						<img src={SearchFriends}></img>
-						<div className={`${styles.friend_font}`}>Search Friends</div>
-						<img src={FriendsArrow}></img>
-					</label>
-				</div> 
-				<div className="drawer-side">
-					<label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-					<ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-						{/* Sidebar content here */}
-						<li><a>Sidebar Item 1</a></li>
-						<li><a>Sidebar Item 2</a></li>
-					</ul>
+           	<input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+			<div className="drawer-content">
+				{/* Page content here */}
+				<label htmlFor="my-drawer-4" className={`${styles.button} ${styles.friend} ${styles.friend_font}` }>
+					<img src={SearchFriends}></img>
+					<div className={`${styles.friend_font}`}>Search Friends</div>
+					<img src={FriendsArrow}></img>
+				</label>
+			</div> 
+			<div className="drawer-side">
+				<label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+				<ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+					{/* Sidebar content here */}
+					<li><Avatar name="James Dinn" img="../../../assets/img_Profile.png" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+					<li><Avatar name="James Dinn" /></li>
+				</ul>
+				<div className={styles.input_container}>
+					<div className="input-group">
+						<input type="text" placeholder="Search…" className={`${styles.input}`} />
+						<img className={styles.btn} src={FriendsAdd} ></img>
+					</div>
 				</div>
 			</div>
+		</div>
 	</div>
 	);
 }
