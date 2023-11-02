e48e72af12f676ed06e17bdcff6a1e1b333289bb
jaeywon <dnsjwodyd@gmail.com>
2023-11-02 21:34:56 +0900

    issue 19

--- src/pages/Profile/Match History/HistoryList.tsx
+++ src/pages/Profile/Match History/HistoryList.tsx
@@ -1,16 +1,16 @@
-import styles from "./HistoryList.module.css"
+import styles from "./HistoryList.module.css";
 
-function HistoryList(props:any) {
-	return (
-		<div className={`${styles.listcontainer}`}>
-			<div className={props.winflag ? styles.listwin : styles.listlose}>
-				{props.winflag ? "Win" : "Lose"}
-			</div>
-			<div className={`${styles.listtime}`}>{props.matchtime}</div>
-			<div className={`${styles.listmatchtext}`}>Match with</div>
-			<div className={`${styles.listname}`}>{props.name}</div>
-		</div>
-	);
+function HistoryList(props: any) {
+  return (
+    <div className={`${styles.listcontainer}`}>
+      <div className={props.winflag ? styles.listwin : styles.listlose}>
+        {props.winflag ? "Win" : "Lose"}
+      </div>
+      <div className={`${styles.listtime}`}>{props.matchtime}</div>
+      <div className={`${styles.listmatchtext}`}>Match with</div>
+      <div className={`${styles.listname}`}>{props.name}</div>
+    </div>
+  );
 }
 
-export default HistoryList
+export default HistoryList;
