f30bc43866cf1126dc1a81483d69107eb9309e5e
sunwchoi <chltjsdn103@naver.com>
2023-12-19 13:30:20 +0900

    Feat: env추가
    1. env추가
    
    resolved #96

--- src/pages/Home/Button_8px.tsx
+++ src/pages/Home/Button_8px.tsx
@@ -2,7 +2,7 @@ import styles from "./Button_8px.module.css";
 
 function Button(props: any) {
   const url =
-    "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-1fd0b93bafae96b52681b16549e5fffd8534a21d46e29b155e7de51e78d0648a&redirect_uri=http%3A%2F%2Flocalhost%3A5173&response_type=code";
+    import.meta.env.VITE_AUTH;
   return (
     <a href={url} className={styles.button}>
       {props.name}
