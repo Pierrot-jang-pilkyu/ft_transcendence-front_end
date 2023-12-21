47f7dde9e4260d9fbb63f559f4823c065a73865c
sunwchoi <chltjsdn103@naver.com>
2023-12-21 13:02:32 +0900

    Fix: Game 페이지 수정
    1.  utils에 freshAxios, freshSocket함수 추가
    2. 인자에 타입작성
    
    resolved #100

--- src/pages/Game/Header/Header.tsx
+++ src/pages/Game/Header/Header.tsx
@@ -9,15 +9,11 @@ function Header()
 		navigate('/Lobby')
 	};
 
-	const handlerButtonLogOut = () => {
-		navigate('/')
-	};
 
 	return (
 		<header className={`${styles.background}`}>
 			<div className={`${styles.container}`}>
 				<button className={`${styles.home} ${styles.button}`} onClick={handlerButton}>Deer Feer</button>
-				<button className={`${styles.logout} ${styles.button}`} onClick={handlerButtonLogOut}>Logout</button>
 			</div>
 		</header>
 	);
